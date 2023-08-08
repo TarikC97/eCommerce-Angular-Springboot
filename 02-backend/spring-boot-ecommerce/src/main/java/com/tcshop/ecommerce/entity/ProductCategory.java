fba496a1ed79e9837bb46a4de04c84ab7d4c6231
unknown <tarikcuric97@gmail.com>
2023-08-09 01:17:53 +0200

    eCommerce Angular-SpringBoot 09-08-2023

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.tcshop.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+//@Data - knowBug(One to Many,Alternative)
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name="id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+}
