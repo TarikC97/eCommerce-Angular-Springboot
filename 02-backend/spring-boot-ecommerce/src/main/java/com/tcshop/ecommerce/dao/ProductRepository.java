ac3481b4a84fceb11b97675d7d0d2022c09f0f19
unknown <tarikcuric97@gmail.com>
2023-08-11 23:41:06 +0200

    eCommerce Angular-Spring 11-08-2023

--- 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.tcshop.ecommerce.dao;
 
 import com.tcshop.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 //Accepts call from localhost 4200.
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    //Query method for category id through param
+    //Behind the scenes spring executes query:
+    //Select * From product where category_id=?
+    Page<Product> findByCategoryId(@RequestParam("id")Long id, Pageable pageable);
 }
