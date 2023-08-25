ae2b5dd545e81c632481e3f8627804f8e89bd9b2
unknown <tarikcuric97@gmail.com>
2023-08-26 01:09:44 +0200

    eCommerce Angular-Springboot 26-08-2023

--- 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/controller/UserController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.tcshop.ecommerce.controller;
 
-
-import com.tcshop.ecommerce.dao.RoleRepository;
 import com.tcshop.ecommerce.dao.UserRepository;
+import com.tcshop.ecommerce.entity.User;
+import com.tcshop.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
     private UserRepository userRepository;
     @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
+    @PostMapping("/register")
+    public User registerNewUser(@RequestBody  User user){
 
+        if(userRepository.existsByEmail(user.getEmail())){
+            throw new IllegalStateException("Email taken");
+        }
+        else{
+            User newUser = new User();
+            newUser.setName(user.getName());
+            newUser.setSurname(user.getSurname());
+            newUser.setRole(user.getRole());
+            newUser.setEmail(user.getEmail());
+            newUser.setPassword(user.getPassword());
+            userRepository.save(newUser);
 
+            return userService.registerNewUser(newUser);
+        }
+    }
+    @PostMapping("/login")
+    public User loginUser(@RequestBody User userData){
+        User user = userRepository.findByEmail(userData.getEmail());
+        if(user.getPassword().equals(userData.getPassword())){
+            return user;
+        }
+        else{
+            throw new IllegalStateException("User doesn't exist!");
+       }
+    }
 }
--- 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/entity/User.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tcshop/ecommerce/entity/User.java
@@ -5,26 +5,24 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Entity
 @Getter
 @Setter
+@Table(name = "user")
 public class User {
     @Id
-    private String userName;
-    private String userFirstName;
-    private String userLastName;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "email")
     private String email;
-    private String userPassword;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_role",
-    joinColumns = {
-            @JoinColumn(name = "user_id")
-        },
-            inverseJoinColumns = {
-                @JoinColumn(name="role_id")
-            }
-    )
-    private Set<Role> role;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private  String role;
+
 }
